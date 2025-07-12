49775d0b81ebb6b9d470fa0d6a8cfe4937fa58d2
Sarojan-stha <myselfsarojan@gmail.com>
2025-07-12 23:37:48 +0545

    single selection accordian

--- src/components/Accordian/Accordian.jsx
+++ src/components/Accordian/Accordian.jsx
@@ -1,5 +1,6 @@
 import data from "./data";
 import React, { useState } from "react";
+import "./accordian.css";
 
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
@@ -10,20 +11,26 @@ const Accordian = () => {
 
   return (
     <>
-      <div className="accordian">
-        {data && data.length > 0 ? (
-          data.map((dataItem) => (
-            <div className="item">
-              <div onClick={() => handleClick(dataItem.id)} className="title">
-                <h3>{dataItem.question}</h3>
-                <span>+</span>
-                {selected === dataItem.id ? <h3>{dataItem.answer}</h3> : ""}
+      <div className="wrapper">
+        <div className="accordian">
+          {data && data.length > 0 ? (
+            data.map((dataItem) => (
+              <div className="item">
+                <div onClick={() => handleClick(dataItem.id)} className="title">
+                  <h3>{dataItem.question}</h3>
+                  <span>+</span>
+                </div>
+                {selected === dataItem.id ? (
+                  <div className="content">{dataItem.answer}</div>
+                ) : (
+                  ""
+                )}
               </div>
-            </div>
-          ))
-        ) : (
-          <div>No Data Found</div>
-        )}
+            ))
+          ) : (
+            <div>No Data Found</div>
+          )}
+        </div>
       </div>
     </>
   );
