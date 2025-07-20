7cb5d8f6f0ba4f338ec3606348123148ee6b9b45
Sarojan-stha <myselfsarojan@gmail.com>
2025-07-20 23:59:08 +0545

    added toggle logic and switch

--- src/components/light-dark-mode/ToggleTheme.jsx
+++ src/components/light-dark-mode/ToggleTheme.jsx
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
 import "./themes.css";
+import { Switch } from "antd";
 
 const ToggleTheme = () => {
   const key = "theme";
   const [theme, setTheme] = useState(localStorage.getItem(key) || "light");
 
-  const handleToggleTheme = () => {
-    setTheme((theme) => (theme === "light" ? "dark" : "light"));
-    localStorage.setItem(key, theme);
+  console.log(theme);
+
+  const handleThemeChange = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem(key, newTheme);
     console.log(key, theme);
   };
 
   return (
-    <div className={theme}>
-      <h1>Toggle Theme</h1>
-      <button onClick={handleToggleTheme}>Toggle</button>
+    <div className="light-dark-mode" data-theme={theme}>
+      <div className="container">
+        <h1>Toogle theme</h1>
+        <button
+          onClick={() => {
+            handleThemeChange();
+          }}
+        >
+          Change Theme
+        </button>
+        <Switch
+          className="toggle-switch"
+          onChange={handleThemeChange}
+          checkedChildren={`${theme} Mode`}
+          unCheckedChildren={`${theme} Mode`}
+        />
+      </div>
     </div>
   );
 };
